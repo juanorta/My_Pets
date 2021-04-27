974c91c08094690e577c3ff12c18642686627e87
juan <1juanorta@gmail.com>
2021-04-27 14:31:11 -0500

    - Added prefixed authorization endpoint, redirection endpoint, UserController - Changed API Base Url and OAUTH2 redirection endpoint to mypetfamily.io

--- my-pets-backend/src/main/java/com/juan/projects/mypets/controller/UserController.java
+++ my-pets-backend/src/main/java/com/juan/projects/mypets/controller/UserController.java
@@ -8,12 +8,13 @@ import com.juan.projects.mypets.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-//@RequestMapping("/api")
+@RequestMapping("/api")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
